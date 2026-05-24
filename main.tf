16f4eb72cd9754e3186b311d46610ed4e66f5ff8
Radnok <87167865+JustKonrad@users.noreply.github.com>
2026-05-24 19:18:44 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-3"
+}
+
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket = "konrad-terraform-demo-bucket"
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.demo_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
