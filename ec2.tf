c4c7ce335233411b90bca2cb4432304f05c18c88
Radnok <87167865+JustKonrad@users.noreply.github.com>
2026-05-25 18:04:12 +0200

    Add security group for EC2 instance

--- ec2.tf
+++ ec2.tf
@@ -6,3 +6,22 @@ resource "aws_instance" "demo_server" {
     Name = "konrad-demo-server"
   }
 }
+
+resource "aws_security_group" "web_sg" {
+  name        = "web-security-group"
+  description = "Allow HTTP and SSH traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
